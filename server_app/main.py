5dd0ae6ce12263e13a65e650947bea7b264b5991
nixis <nixissmtp@gmail.com>
2020-11-07 22:10:50 +0200

    Ending building flask template

---
+++ server_app/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "<h2>Hello, from Heroku</h2>"
---
+++ wsgi.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from server_app.main import app
+
+if __name__ == "__main__":
+    app.run()
